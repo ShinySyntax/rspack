ShinySyntax/rspack

commit 2ce399f2eb1f970efe3058c6e44dc4460c620529

fixture(dev-server): close middleware at dev-server

packages/rspack/src/compiler.ts
@@ -21,10 +21,10 @@ import { createSourceFromRaw } from "./util/createSource";
 import ResolverFactory from "./ResolverFactory";
 
 class EntryPlugin {
-	apply() { }
+	apply() {}
 }
 class HotModuleReplacementPlugin {
-	apply() { }
+	apply() {}
 }
 type CompilationParams = Record<string, any>;
 class Compiler {
@@ -205,7 +205,7 @@ class Compiler {
 	 * @param value
 	 * @returns
 	 */
-	#done(statsJson: binding.JsStatsCompilation) { }
+	#done(statsJson: binding.JsStatsCompilation) {}
 
 	async #processAssets() {
 		await this.compilation.hooks.processAssets.promise(
@@ -340,10 +340,12 @@ class Compiler {
 			this.inputFileSystem.purge();
 		}
 	}
+
 	/**
 	 * @todo
 	 */
 	close(callback) {
+		this.#_instance = null;
 		callback();
 	}
 	emitAssets(compilation: Compilation, callback) {

packages/rspack-dev-server/src/server.ts
@@ -225,15 +225,30 @@ export class RspackDevServer {
 	}
 
 	async stop(): Promise<void> {
+		this.compiler.close(() => {});
 		await Promise.all(this.staticWatchers.map(watcher => watcher.close()));
-		this.middleware = null;
 		this.staticWatchers = [];
+
+		if (this.middleware) {
+			await new Promise((resolve, reject) => {
+				this.middleware.close((error: Error) => {
+					if (error) {
+						reject(error);
+						return;
+					}
+					resolve(undefined);
+				});
+			});
+		}
+		this.middleware = null;
+
 		if (this.server) {
 			this.server.close();
 		}
 		if (this.webSocketServer) {
 			await new Promise(resolve => {
 				this.webSocketServer.implementation.close(() => {
+					this.webSocketServer = null;
 					resolve(void 0);
 				});
 				for (const client of this.webSocketServer.clients) client.terminate();

commit 42f6fbd80999255a9dcc56310d573033729a6a1d

fix: watching loader fileDependencies (#1519)
- fix watch loader fileDependencies broken by #1482
- fix create_chunk_assets cache validation
- add watch build and watchCases tests

crates/rspack_core/src/cache/occasion/create_chunk_assets.rs
@@ -36,12 +36,14 @@ impl CreateChunkAssetsOccasion {
       .get_chunk_graph_chunk(&chunk.ukey)
       .modules;
     let is_cache_valid = modules.into_iter().all(|module_id| {
-      compilation
-        .module_graph
-        .module_by_identifier(module_id)
-        .and_then(|m| m.as_normal_module())
-        .map(|m| matches!(m.ast_or_source(), NormalModuleAstOrSource::Unbuild))
-        .is_some()
+      matches!(
+        compilation
+          .module_graph
+          .module_by_identifier(module_id)
+          .and_then(|m| m.as_normal_module())
+          .map(|m| matches!(m.ast_or_source(), NormalModuleAstOrSource::Unbuild)),
+        Some(true)
+      )
     });
 
     if is_cache_valid {

packages/rspack/src/compiler.ts
@@ -35,7 +35,7 @@ class Compiler {
 	root: Compiler;
 	resolverFactory: ResolverFactory;
 	infrastructureLogger: any;
-	watcher: FSWatcher;
+	watching: Watching;
 	outputPath: string;
 	name: string;
 	inputFileSystem: any;
@@ -331,8 +331,8 @@ class Compiler {
 			ignoreInitial: true,
 			...options
 		});
-		const watching = new Watching(this, watcher, handler);
-		return watching;
+		this.watching = new Watching(this, watcher, handler);
+		return this.watching;
 	}
 
 	purgeInputFileSystem() {
@@ -346,6 +346,13 @@ class Compiler {
 	 */
 	close(callback) {
 		this.#_instance = null;
+		if (this.watching) {
+			// When there is still an active watching, close this first
+			this.watching.close(() => {
+				this.close(callback);
+			});
+			return;
+		}
 		callback();
 	}
 	emitAssets(compilation: Compilation, callback) {

packages/rspack/src/config/module.ts
@@ -267,17 +267,22 @@ function composeJsUse(
 				}
 
 				const getResolveContext = () => {
+					// FIXME: resolve's fileDependencies will includes lots of dir, '/', etc.
 					return {
 						fileDependencies: {
 							add: d => {
-								// FIXME: This will cause build hang, so we skip it
+								// loaderContext.addDependency(d)
 							}
 						},
 						contextDependencies: {
-							add: d => loaderContext.addContextDependency(d)
+							add: d => {
+								// loaderContext.addContextDependency(d)
+							}
 						},
 						missingDependencies: {
-							add: d => loaderContext.addMissingDependency(d)
+							add: d => {
+								// loaderContext.addMissingDependency(d)
+							}
 						}
 					};
 				};
@@ -586,7 +591,8 @@ function composeJsUse(
 				content = loaderResult.content ?? content;
 				sourceMap = loaderResult.sourceMap ?? sourceMap;
 				cacheable = loaderResult.cacheable ?? cacheable;
-				compiler.watcher?.add([
+
+				compiler.watching?.watch([
 					data.resourcePath,
 					...loaderResult.fileDependencies,
 					...loaderResult.contextDependencies,

packages/rspack/src/config/watch.ts
@@ -1,10 +1,8 @@
-export type Watch = {
-	ignored?: string[];
-};
+import type { WatchOptions } from "chokidar";
 
-export type ResolvedWatch = {
-	ignored: string[];
-};
+export type Watch = WatchOptions;
+
+export type ResolvedWatch = WatchOptions;
 
 export function resolveWatchOption(watch: Watch = {}): ResolvedWatch {
 	const ignored = watch.ignored ?? [

packages/rspack/src/watching.ts
@@ -3,17 +3,19 @@ import type { FSWatcher } from "chokidar";
 import { Stats } from ".";
 
 class Watching {
-	wather: FSWatcher;
+	watcher: FSWatcher;
+	compiler: Compiler;
+
 	constructor(
 		compiler: Compiler,
 		watcher: FSWatcher,
 		handler?: (error?: Error, stats?: Stats) => void
 	) {
-		this.wather = watcher;
+		this.watcher = watcher;
+		this.compiler = compiler;
 
-		const begin = Date.now();
-
-		watcher.on("ready", () => {
+		const build = () => {
+			const begin = Date.now();
 			compiler.build((error, rawStats) => {
 				if (error && handler) {
 					return handler(error);
@@ -28,7 +30,9 @@ class Watching {
 					console.log("build success, time cost", Date.now() - begin, "ms");
 				});
 			});
-		});
+		};
+
+		watcher.on("ready", build);
 
 		let pendingChangedFilepaths = new Set<string>();
 		let isBuildFinished = true;
@@ -79,12 +83,21 @@ class Watching {
 				});
 			};
 
-			rebuildWithFilepaths([...pendingChangedFilepaths, changedFilepath]);
+			if (compiler.options.devServer) {
+				rebuildWithFilepaths([...pendingChangedFilepaths, changedFilepath]);
+			} else {
+				build();
+			}
 		});
 	}
 
+	watch(paths: string[]) {
+		this.watcher.add(paths);
+	}
+
 	close(callback?: () => void) {
-		this.wather.close().then(callback);
+		this.compiler.watching = undefined;
+		this.watcher.close().then(callback);
 	}
 
 	invalidate() {}

packages/rspack/tests/WatchTestCases.template.ts
@@ -1,0 +1,447 @@
+import path from "path";
+import fs from "graceful-fs";
+import vm from "vm";
+import rimraf from "rimraf";
+import checkArrayExpectation from "./checkArrayExpectation";
+import createLazyTestEnv from "./helpers/createLazyTestEnv";
+import { remove } from "./helpers/remove";
+import prepareOptions from "./helpers/prepareOptions";
+import deprecationTracking from "./helpers/deprecationTracking";
+import FakeDocument from "./helpers/FakeDocument";
+import { rspack, RspackOptions } from "../src";
+
+function copyDiff(src, dest, initial) {
+	if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+	const files = fs.readdirSync(src);
+	files.forEach(filename => {
+		const srcFile = path.join(src, filename);
+		const destFile = path.join(dest, filename);
+		const directory = fs.statSync(srcFile).isDirectory();
+		if (directory) {
+			copyDiff(srcFile, destFile, initial);
+		} else {
+			var content = fs.readFileSync(srcFile);
+			if (/^DELETE\s*$/.test(content.toString("utf-8"))) {
+				fs.unlinkSync(destFile);
+			} else if (/^DELETE_DIRECTORY\s*$/.test(content.toString("utf-8"))) {
+				rimraf.sync(destFile);
+			} else {
+				fs.writeFileSync(destFile, content);
+				if (initial) {
+					const longTimeAgo = Date.now() - 1000 * 60 * 60 * 24;
+					fs.utimesSync(
+						destFile,
+						Date.now() - longTimeAgo,
+						Date.now() - longTimeAgo
+					);
+				}
+			}
+		}
+	});
+}
+
+export const describeCases = (config: any) => {
+	describe(config.name, () => {
+		if (process.env.NO_WATCH_TESTS) {
+			it.skip("long running tests excluded", () => {});
+			return;
+		}
+
+		const casesPath = path.join(__dirname, config.casePath);
+		let categories = fs.readdirSync(casesPath).map(cat => {
+			return {
+				name: cat,
+				tests: fs
+					.readdirSync(path.join(casesPath, cat))
+					.filter(folder => folder.indexOf("_") < 0)
+					.filter(testName => {
+						const testDirectory = path.join(casesPath, cat, testName);
+						const filterPath = path.join(testDirectory, "test.filter.js");
+						if (fs.existsSync(filterPath) && !require(filterPath)(config)) {
+							describe.skip(testName, () => it("filtered", () => {}));
+							return false;
+						}
+						return true;
+					})
+					.sort()
+			};
+		});
+		beforeAll(() => {
+			let dest = path.join(__dirname, "js");
+			if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+			dest = path.join(__dirname, "js", config.name + "-src");
+			if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+		});
+		categories.forEach(category => {
+			beforeAll(() => {
+				const dest = path.join(
+					__dirname,
+					"js",
+					config.name + "-src",
+					category.name
+				);
+				if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+			});
+			describe(category.name, () => {
+				category.tests.forEach(testName => {
+					describe(testName, () => {
+						const tempDirectory = path.join(
+							__dirname,
+							"js",
+							config.name + "-src",
+							category.name,
+							testName
+						);
+						const testDirectory = path.join(casesPath, category.name, testName);
+						const runs = fs
+							.readdirSync(testDirectory)
+							.sort()
+							.filter(name => {
+								return fs
+									.statSync(path.join(testDirectory, name))
+									.isDirectory();
+							})
+							.map(name => ({ name } as any));
+
+						beforeAll(done => {
+							rimraf(tempDirectory, done);
+						});
+
+						it(
+							testName + " should compile",
+							done => {
+								const outputDirectory = path.join(
+									__dirname,
+									"js",
+									config.name,
+									category.name,
+									testName
+								);
+
+								rimraf.sync(outputDirectory);
+
+								let options: RspackOptions = {};
+								const configPath = path.join(
+									testDirectory,
+									"webpack.config.js"
+								);
+								if (fs.existsSync(configPath)) {
+									options = prepareOptions(require(configPath), {
+										testPath: outputDirectory,
+										srcPath: tempDirectory
+									});
+								}
+								const applyConfig = (options, idx) => {
+									if (!options.mode) options.mode = "development";
+									if (!options.context) {
+										options.context = tempDirectory;
+									} else if (!path.isAbsolute(options.context)) {
+										options.context = path.join(tempDirectory, options.context);
+									}
+									if (!options.entry) options.entry = "./index.js";
+									// TODO: shoud be `if (!options.target) options.target = "async-node";`
+									if (!options.target) {
+										options.target = "node";
+										options.externals = {
+											fs: "fs",
+											path: "path"
+										};
+										options.externalsType = "node-commonjs";
+									}
+									if (!options.output) options.output = {};
+									if (!options.output.path)
+										options.output.path = outputDirectory;
+									// if (typeof options.output.pathinfo === "undefined")
+									// 	options.output.pathinfo = true;
+									if (!options.output.filename)
+										options.output.filename = "bundle.js";
+									// if (options.cache && options.cache.type === "filesystem") {
+									// 	const cacheDirectory = path.join(tempDirectory, ".cache");
+									// 	options.cache.cacheDirectory = cacheDirectory;
+									// 	options.cache.name = `config-${idx}`;
+									// }
+									if (config.experiments) {
+										if (!options.experiments) options.experiments = {};
+										for (const key of Object.keys(config.experiments)) {
+											if (options.experiments[key] === undefined)
+												options.experiments[key] = config.experiments[key];
+										}
+									}
+									if (config.optimization) {
+										if (!options.optimization) options.optimization = {};
+										for (const key of Object.keys(config.optimization)) {
+											if (options.optimization[key] === undefined)
+												options.optimization[key] = config.optimization[key];
+										}
+									}
+								};
+								if (Array.isArray(options)) {
+									options.forEach(applyConfig);
+								} else {
+									applyConfig(options, 0);
+								}
+
+								const state = {};
+								let runIdx = 0;
+								let waitMode = false;
+								let run: any = runs[runIdx];
+								let triggeringFilename;
+								let lastHash = "";
+								const currentWatchStepModule = require("./helpers/currentWatchStep");
+								let compilationFinished = done;
+								currentWatchStepModule.step = run.name;
+								copyDiff(
+									path.join(testDirectory, run.name),
+									tempDirectory,
+									true
+								);
+
+								setTimeout(() => {
+									const deprecationTracker = deprecationTracking.start();
+									const compiler = rspack(options);
+									// compiler.hooks.invalid.tap(
+									// 	"WatchTestCasesTest",
+									// 	(filename, mtime) => {
+									// 		triggeringFilename = filename;
+									// 	}
+									// );
+									compiler.watch(
+										{
+											usePolling: true
+											// aggregateTimeout: 1000
+										},
+										(err, stats) => {
+											if (err) return compilationFinished(err);
+											if (!stats) {
+												return compilationFinished(
+													new Error("No stats reported from Compiler")
+												);
+											}
+											if (stats.hash === lastHash) return;
+											lastHash = stats.hash;
+											if (run.done && lastHash !== stats.hash) {
+												return compilationFinished(
+													new Error(
+														"Compilation changed but no change was issued " +
+															lastHash +
+															" != " +
+															stats.hash +
+															" (run " +
+															runIdx +
+															")\n" +
+															"Triggering change: " +
+															triggeringFilename
+													)
+												);
+											}
+											if (waitMode) return;
+											run.done = true;
+											run.stats = stats;
+											if (err) return compilationFinished(err);
+											const statOptions = {
+												preset: "verbose" as const,
+												// cached: true,
+												// cachedAssets: true,
+												// cachedModules: true,
+												colors: false
+											};
+											fs.mkdirSync(outputDirectory, { recursive: true });
+											fs.writeFileSync(
+												path.join(
+													outputDirectory,
+													`stats.${runs[runIdx] && runs[runIdx].name}.txt`
+												),
+												stats.toString(statOptions),
+												"utf-8"
+											);
+											const jsonStats = stats.toJson({
+												errors: true
+											});
+											if (
+												checkArrayExpectation(
+													path.join(testDirectory, run.name),
+													jsonStats,
+													"error",
+													"Error",
+													compilationFinished
+												)
+											)
+												return;
+											if (
+												checkArrayExpectation(
+													path.join(testDirectory, run.name),
+													jsonStats,
+													"warning",
+													"Warning",
+													compilationFinished
+												)
+											)
+												return;
+
+											const globalContext = {
+												console: console,
+												expect: expect,
+												setTimeout,
+												clearTimeout,
+												document: new FakeDocument()
+											};
+
+											function _require(currentDirectory, module) {
+												if (Array.isArray(module) || /^\.\.?\//.test(module)) {
+													let fn;
+													let content;
+													let p;
+													if (Array.isArray(module)) {
+														p = path.join(currentDirectory, module[0]);
+														content = module
+															.map(arg => {
+																p = path.join(currentDirectory, arg);
+																return fs.readFileSync(p, "utf-8");
+															})
+															.join("\n");
+													} else {
+														p = path.join(currentDirectory, module);
+														content = fs.readFileSync(p, "utf-8");
+													}
+													if (
+														options.target === "web" ||
+														options.target === "webworker"
+													) {
+														fn = vm.runInNewContext(
+															"(function(require, module, exports, __dirname, __filename, it, WATCH_STEP, STATS_JSON, STATE, expect, window, self) {" +
+																'function nsObj(m) { Object.defineProperty(m, Symbol.toStringTag, { value: "Module" }); return m; }' +
+																content +
+																"\n})",
+															globalContext,
+															p
+														);
+													} else {
+														fn = vm.runInThisContext(
+															"(function(require, module, exports, __dirname, __filename, it, WATCH_STEP, STATS_JSON, STATE, expect) {" +
+																"global.expect = expect;" +
+																'function nsObj(m) { Object.defineProperty(m, Symbol.toStringTag, { value: "Module" }); return m; }' +
+																content +
+																"\n})",
+															p
+														);
+													}
+													const m = {
+														exports: {}
+													};
+													fn.call(
+														m.exports,
+														_require.bind(null, path.dirname(p)),
+														m,
+														m.exports,
+														path.dirname(p),
+														p,
+														run.it,
+														run.name,
+														jsonStats,
+														state,
+														expect,
+														globalContext,
+														globalContext
+													);
+													return module.exports;
+												} else if (
+													testConfig.modules &&
+													module in testConfig.modules
+												) {
+													return testConfig.modules[module];
+												} else return jest.requireActual(module);
+											}
+
+											let testConfig: any = {};
+											try {
+												// try to load a test file
+												testConfig = require(path.join(
+													testDirectory,
+													"test.config.js"
+												));
+											} catch (e) {
+												// empty
+											}
+
+											if (testConfig.noTests)
+												return process.nextTick(compilationFinished);
+											_require(
+												outputDirectory,
+												testConfig.bundlePath || "./bundle.js"
+											);
+
+											if (run.getNumberOfTests() < 1)
+												return compilationFinished(
+													new Error("No tests exported by test case")
+												);
+
+											run.it(
+												"should compile the next step",
+												done => {
+													runIdx++;
+													if (runIdx < runs.length) {
+														run = runs[runIdx];
+														waitMode = true;
+														setTimeout(() => {
+															waitMode = false;
+															compilationFinished = done;
+															currentWatchStepModule.step = run.name;
+															copyDiff(
+																path.join(testDirectory, run.name),
+																tempDirectory,
+																false
+															);
+														}, 1500);
+													} else {
+														const deprecations = deprecationTracker();
+														if (
+															checkArrayExpectation(
+																testDirectory,
+																{ deprecations },
+																"deprecation",
+																"Deprecation",
+																done
+															)
+														) {
+															compiler.close(() => {});
+															return;
+														}
+														compiler.close(done);
+													}
+												},
+												45000
+											);
+
+											compilationFinished();
+										}
+									);
+								}, 300);
+							},
+							45000
+						);
+
+						for (const run of runs) {
+							const { it: _it, getNumberOfTests } = createLazyTestEnv(
+								10000,
+								run.name
+							);
+							run.it = _it;
+							run.getNumberOfTests = getNumberOfTests;
+							it(`${run.name} should allow to read stats`, done => {
+								if (run.stats) {
+									run.stats.toString({ all: true });
+									run.stats = undefined;
+								}
+								done();
+							});
+						}
+
+						afterAll(() => {
+							remove(tempDirectory);
+						});
+					});
+				});
+			});
+		});
+	});
+};

packages/rspack/tests/WatchTestCases.test.ts
@@ -1,0 +1,6 @@
+import { describeCases } from "./WatchTestCases.template";
+
+describeCases({
+	name: "WatchTestCases",
+	casePath: "watchCases"
+});

packages/rspack/tests/helpers/FakeDocument.js
@@ -1,0 +1,203 @@
+const fs = require("fs");
+const path = require("path");
+
+const getPropertyValue = function (property) {
+	return this[property];
+};
+
+module.exports = class FakeDocument {
+	constructor(basePath) {
+		this.head = this.createElement("head");
+		this.body = this.createElement("body");
+		this.baseURI = "https://test.cases/path/index.html";
+		this._elementsByTagName = new Map([
+			["head", [this.head]],
+			["body", [this.body]]
+		]);
+		this._basePath = basePath;
+	}
+
+	createElement(type) {
+		return new FakeElement(this, type, this._basePath);
+	}
+
+	_onElementAttached(element) {
+		const type = element._type;
+		let list = this._elementsByTagName.get(type);
+		if (list === undefined) {
+			list = [];
+			this._elementsByTagName.set(type, list);
+		}
+		list.push(element);
+	}
+
+	_onElementRemoved(element) {
+		const type = element._type;
+		let list = this._elementsByTagName.get(type);
+		const idx = list.indexOf(element);
+		list.splice(idx, 1);
+	}
+
+	getElementsByTagName(name) {
+		return this._elementsByTagName.get(name) || [];
+	}
+
+	getComputedStyle(element) {
+		const style = { getPropertyValue };
+		const links = this.getElementsByTagName("link");
+		for (const link of links) {
+			for (const rule of link.sheet.cssRules) {
+				if (rule.selectorText === element._type) {
+					Object.assign(style, rule.style);
+				}
+			}
+		}
+		return style;
+	}
+};
+
+class FakeElement {
+	constructor(document, type, basePath) {
+		this._document = document;
+		this._type = type;
+		this._children = [];
+		this._attributes = Object.create(null);
+		this._src = undefined;
+		this._href = undefined;
+		this.parentNode = undefined;
+		this.sheet = type === "link" ? new FakeSheet(this, basePath) : undefined;
+	}
+
+	appendChild(node) {
+		this._document._onElementAttached(node);
+		this._children.push(node);
+		node.parentNode = this;
+		if (node._type === "link") {
+			setTimeout(() => {
+				if (node.onload) node.onload({ type: "load", target: node });
+			}, 100);
+		}
+	}
+
+	removeChild(node) {
+		const idx = this._children.indexOf(node);
+		if (idx >= 0) {
+			this._children.splice(idx, 1);
+			this._document._onElementRemoved(node);
+			node.parentNode = undefined;
+		}
+	}
+
+	setAttribute(name, value) {
+		this._attributes[name] = value;
+	}
+
+	removeAttribute(name) {
+		delete this._attributes[name];
+	}
+
+	getAttribute(name) {
+		return this._attributes[name];
+	}
+
+	_toRealUrl(value) {
+		if (/^\//.test(value)) {
+			return `https://test.cases${value}`;
+		} else if (/^\.\.\//.test(value)) {
+			return `https://test.cases${value.slice(2)}`;
+		} else if (/^\.\//.test(value)) {
+			return `https://test.cases/path${value.slice(1)}`;
+		} else if (/^\w+:\/\//.test(value)) {
+			return value;
+		} else if (/^\/\//.test(value)) {
+			return `https:${value}`;
+		} else {
+			return `https://test.cases/path/${value}`;
+		}
+	}
+
+	set src(value) {
+		if (this._type === "script") {
+			this._src = this._toRealUrl(value);
+		}
+	}
+
+	get src() {
+		return this._src;
+	}
+
+	set href(value) {
+		if (this._type === "link") {
+			this._href = this._toRealUrl(value);
+		}
+	}
+
+	get href() {
+		return this._href;
+	}
+}
+
+class FakeSheet {
+	constructor(element, basePath) {
+		this._element = element;
+		this._basePath = basePath;
+	}
+
+	get cssRules() {
+		const walkCssTokens = require("../../lib/css/walkCssTokens");
+		const rules = [];
+		let currentRule = { getPropertyValue };
+		let selector = undefined;
+		let last = 0;
+		const processDeclaration = str => {
+			const colon = str.indexOf(":");
+			if (colon > 0) {
+				const property = str.slice(0, colon).trim();
+				const value = str.slice(colon + 1);
+				currentRule[property] = value;
+			}
+		};
+		let css = fs.readFileSync(
+			path.resolve(
+				this._basePath,
+				this._element.href.replace(/^https:\/\/test\.cases\/path\//, "")
+			),
+			"utf-8"
+		);
+		css = css.replace(/@import url\("([^"]+)"\);/g, (match, url) => {
+			return fs.readFileSync(
+				path.resolve(
+					this._basePath,
+					url.replace(/^https:\/\/test\.cases\/path\//, "")
+				),
+				"utf-8"
+			);
+		});
+		walkCssTokens(css, {
+			isSelector() {
+				return selector === undefined;
+			},
+			leftCurlyBracket(source, start, end) {
+				if (selector === undefined) {
+					selector = source.slice(last, start).trim();
+					last = end;
+				}
+				return end;
+			},
+			rightCurlyBracket(source, start, end) {
+				processDeclaration(source.slice(last, start));
+				last = end;
+				rules.push({ selectorText: selector, style: currentRule });
+				selector = undefined;
+				currentRule = { getPropertyValue };
+				return end;
+			},
+			semicolon(source, start, end) {
+				processDeclaration(source.slice(last, start));
+				last = end;
+				return end;
+			}
+		});
+		return rules;
+	}
+}

packages/rspack/tests/helpers/currentWatchStep.js
@@ -1,0 +1,1 @@
+exports.step = undefined;

packages/rspack/tests/helpers/prepareOptions.js
@@ -1,0 +1,30 @@
+"use strict";
+
+const handleExport = options => {
+	const isES6DefaultExported =
+		typeof options === "object" &&
+		options !== null &&
+		typeof options.default !== "undefined";
+	options = isES6DefaultExported ? options.default : options;
+	return options;
+};
+
+const handleFunction = (options, argv) => {
+	if (typeof options === "function") {
+		options = options(argv.env, argv);
+	}
+	return options;
+};
+
+module.exports = (options, argv) => {
+	argv = argv || {};
+
+	options = handleExport(options);
+
+	if (Array.isArray(options)) {
+		options = options.map(_options => handleFunction(_options, argv));
+	} else {
+		options = handleFunction(options, argv);
+	}
+	return options;
+};

packages/rspack/tests/helpers/remove.js
@@ -1,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports.remove = function remove(src) {
+	if (!fs.existsSync(src)) return;
+	const files = fs.readdirSync(src);
+	files.forEach(filename => {
+		const srcFile = path.join(src, filename);
+		const directory = fs.statSync(srcFile).isDirectory();
+		if (directory) {
+			remove(srcFile);
+		} else {
+			fs.unlinkSync(srcFile);
+		}
+	});
+};

packages/rspack/tests/watchCases/context/out-of-context/0/bar.less
@@ -1,0 +1,1 @@
+@step: 0;

packages/rspack/tests/watchCases/context/out-of-context/0/src/foo.less
@@ -1,0 +1,5 @@
+@import '../bar.less';
+
+.foo {
+  step: @step;
+}

packages/rspack/tests/watchCases/context/out-of-context/0/src/index.js
@@ -1,0 +1,10 @@
+require("./foo.less");
+
+const fs = require("fs");
+const path = require("path");
+
+it("should detect changes for loader fileDependency which out of context", function () {
+	const less = fs.readFileSync(path.resolve(__dirname, "bundle.css"), "utf-8");
+	const step = /step: (.*);/.exec(less)[1];
+	expect(step).toBe(WATCH_STEP);
+});

packages/rspack/tests/watchCases/context/out-of-context/1/bar.less
@@ -1,0 +1,1 @@
+@step: 1;

packages/rspack/tests/watchCases/context/out-of-context/2/bar.less
@@ -1,0 +1,1 @@
+@step: 2;

packages/rspack/tests/watchCases/context/out-of-context/webpack.config.js
@@ -1,0 +1,18 @@
+module.exports = {
+	entry: "./index",
+	context: "./src",
+	module: {
+		rules: [
+			{
+				test: /\.less$/,
+				use: [
+					{
+						loader: "less-loader"
+					}
+				],
+				type: "css"
+			}
+		]
+	},
+	cache: false // FIXME: build cache validate bug which needs fileDependencies on rust side
+};

packages/rspack/tests/watchCases/simple/production/0/changing-module.js
@@ -1,0 +1,1 @@
+export default "0";

packages/rspack/tests/watchCases/simple/production/0/index.js
@@ -1,0 +1,5 @@
+import module from "./changing-module";
+
+it("should watch for changes", function () {
+	expect(module).toBe(WATCH_STEP);
+});

packages/rspack/tests/watchCases/simple/production/1/changing-module.js
@@ -1,0 +1,1 @@
+export default "1";

packages/rspack/tests/watchCases/simple/production/2/changing-module.js
@@ -1,0 +1,1 @@
+export default "2";

packages/rspack/tests/watchCases/simple/production/webpack.config.js
@@ -1,0 +1,4 @@
+/** @type {import("../../../../dist").Configuration} */
+module.exports = {
+	mode: "production"
+};

packages/rspack/tests/watchCases/simple/simple/0/changing-file.js
@@ -1,0 +1,1 @@
+module.exports = "0";

packages/rspack/tests/watchCases/simple/simple/0/index.js
@@ -1,0 +1,3 @@
+it("should watch for changes", function () {
+	expect(require("./changing-file")).toBe(WATCH_STEP);
+});

packages/rspack/tests/watchCases/simple/simple/1/changing-file.js
@@ -1,0 +1,1 @@
+module.exports = "1";

packages/rspack/tests/watchCases/simple/simple/2/changing-file.js
@@ -1,0 +1,1 @@
+module.exports = "2";